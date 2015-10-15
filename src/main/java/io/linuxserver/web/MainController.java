19885873fc3a13b92f4f687187d2c2b19551c949
Josh Stark <jagfin1@gmail.com>
2015-10-15 19:38:54 +0100

    Added log configuration

--- src/main/java/io/linuxserver/web/MainController.java
+++ src/main/java/io/linuxserver/web/MainController.java
@@ -1,14 +1,10 @@
 package io.linuxserver.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/")
     public String index() {
